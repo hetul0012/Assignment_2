9e1bb83b439a1d4a9d647c49bd2f8b366e4fe54e
hetul0012 <hetulss3@gmail.com>
2025-02-21 16:54:37 -0500

    Add project files.

---
+++ Controllers/J1.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment_2.Controllers
+{
+    [Route("api/J1")]
+    [ApiController]
+    public class J1 : ControllerBase
+    {
+
+        [HttpPost(template: "DelivEDroid")]
+        [Consumes("application/x-www-form-urlencoded")]
+
+        public int DelivEDroid(int Collisions, int Deliveries)
+        {
+            // <summary>
+            // Deliv-e-droid, a robot droid has to deliver packages while avoiding obstacles.
+            // calculated final score based on deliveries and collisions with obstacles
+            // </summary>
+            // <param name="collisions">The number of collisions the robot has with obstacles.</param>
+            // <param name="deliveries">The number of packages the robot successfully delivers.</param>
+            // <returns>he final score based on the deliveries and collisions</returns>
+            // <example>
+            //   GET : localhost:7151/api/J1/DelivEDroid?Collisions=2&Deliveries=5
+            // </example>
+            //<result>730</result>
+            // <example>
+            //   GET : localhost:7151/api/J1/DelivEDroid?Collisions=10&Deliveries=0
+            // </example>
+            //<result>-100</result>
+            // <example>
+            //   GET : localhost:7151/api/J1/DelivEDroid?Collisions=3&Deliveries=2
+            // </example>
+            //<result>70</result>
+
+            int Final_Score = (Deliveries * 50) - (Collisions * 10);
+
+            // Add bonus if Deliveries > Collisions
+            if (Deliveries > Collisions)
+            {
+                Final_Score = Final_Score + 500;
+            }
+
+            return Final_Score;
+        }
+
+        [HttpPost("SpecialDay")]
+        [Consumes("application/x-www-form-urlencoded")]
+        public string SpecialDay([FromForm] int month, [FromForm] int day)
+        {
+
+            // <summary>
+            // asks the user for a numerical month and numerical day of the month.
+            // determines whether that date occurs before, after, or on February 18.
+            // </summary>
+
+            // <param name="month">The month of the date. </param>
+            // <param day="deliveries">The day of the month.</param>
+            // <returns>A string indicating whether the date is "Before", "After", or "Special".</returns>
+            // <example>
+            //   POST : localhost:7151/api/J1/SpecialDay
+            //   month=1&day=7
+            // </example>
+            //<result>Before</result>
+            // <example>
+            //   POST : localhost:7151/api/J1/SpecialDay
+            //   month=8&day=31
+            // </example>
+            //<result>After</result>
+            // <example>
+            //  POST : localhost:7151/api/J1/SpecialDay
+            //   month=2&day=18
+            // </example>
+            //<result>Special</result>
+
+
+            if (month < 2 && day < 18)
+            {
+                return "Before";
+            }
+            else if (month > 2 && day > 18)
+            {
+                return "After";
+            }
+            else
+            {
+                return "Special";
+            }
+
+        }
+
+    }
+}
---
+++ Controllers/J2.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment_2.Controllers
+{
+    [Route("api/J2")]
+    [ApiController]
+    public class J2 : ControllerBase
+    {
+
+
+        [HttpGet("ChiliPeppers")]
+        public IActionResult ChiliPeppers([FromQuery] string ingredientsName)
+        {
+            // <summary>
+            // calculates the total spiciness of a list of chili peppers
+            // </summary>
+
+            // <param name="ingredientsName">string of pepper name </param>
+
+            // <returns>Returns the total spiciness (in SHU) as an integer</returns>
+            // <example>
+            //   GET : localhost:7151/api/J2/ChiliPeppers?ingredientsName=Poblano%2C%20Cayenne%2CThai%2CPoblano
+            // </example>
+            //<result>118000</result>
+            // <example>
+            //   GET : localhost:7151/api/J2/ChiliPeppers?ingredientsName=Habanero%20%2CHabanero%2CHabanero%2CHabanero%2CHabanero
+            // </example>
+            //<result>625000</result>
+            // <example>
+            //   GET : localhost:7151/api/J2/ChiliPeppers?ingredientsName=Poblano%2C%20Mirasol%2CSerrano%2CCayenne%2CThai%2CHabanero%2CSerrano
+            // </example>
+            //<result>278500</result>
+
+            Dictionary<string, int> PepperSHU = new Dictionary<string, int>()
+            {
+                { "Poblano", 1500 },
+                { "Mirasol", 6000 },
+                { "Serrano", 15500 },
+                { "Cayenne", 40000 },
+                { "Thai", 75000 },
+                { "Habanero", 125000 }
+            };
+
+            string[] peppers = ingredientsName.Split(',');
+            int TotalSpiciness = 0;
+
+            for (int i = 0; i < peppers.Length; i++)
+            {
+                string pepperName = peppers[i].Trim();
+                if (PepperSHU.ContainsKey(pepperName))
+                {
+                    TotalSpiciness = TotalSpiciness + PepperSHU[pepperName];
+                }
+                else
+                {
+                    return BadRequest("Invalid Pepper Name: " + pepperName);
+                }
+            }
+
+            return Ok(TotalSpiciness);
+        }
+
+        [HttpGet("FaceMood")]
+        public IActionResult FaceMood([FromQuery] string Mood)
+        {
+
+            // <summary>
+            // A string containing emoticons to determine the overall mood
+            // </summary>
+
+            // <param name="Mood">A string containing emoticons such as ":-)" (happy) and ":-(" (sad).</param>
+            // <param name = "happyCount" > The count of happy emoticons found in the input string.</param>
+            // <param name= "sadCount">The count of sad emoticons found in the input string.</param>
+            // <returns>Returns a string indicating the mood like happy , sad, unsure, and none</returns>
+            // <example>
+            //   GET : localhost:7151/api/J2/FaceMood?Mood=How%20are%20you%20%3A-%29%20doing%20%3A-%28%20today%20%3A-%29%3F
+            // </example>
+            //<result>happy</result>
+            // <example>
+            //   GET : localhost:7151/api/J2/FaceMood?Mood=%3A%29
+            // </example>
+            //<result>none</result>
+            // <example>
+            //   GET : localhost:7151/api/J2/FaceMood?Mood=This%3A-%28is%20str%3A-%28%3A-%28ange%20te%3A-%29xt.
+            // </example>
+            //<result>sad</result>
+
+            int happyCount = 0;
+            int sadCount = 0;
+
+
+            for (int i = 0; i < Mood.Length - 2; i++)
+            {
+                string sub = Mood.Substring(i, 3);
+
+
+                if (sub == ":-)")
+                {
+                    happyCount++;
+                }
+                else if (sub == ":-(")
+                {
+                    sadCount++;
+                }
+            }
+
+
+            if (happyCount == 0 && sadCount == 0)
+            {
+                return Ok("none");
+            }
+            else if (happyCount == sadCount)
+            {
+                return Ok("unsure");
+            }
+            else if (happyCount > sadCount)
+            {
+                return Ok("happy");
+            }
+            else
+            {
+                return Ok("sad");
+            }
+        }
+
+    }
+}
